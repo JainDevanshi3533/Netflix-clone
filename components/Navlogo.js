d377a3413469edff1a80c32bebad05a3aa40a349
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-01-13 17:09:56 +0530

    stying for signin page

--- components/Navlogo.js
+++ components/Navlogo.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import logo from '../Netflix-images/logo.png'
+import logo from '../Netflix-images/logo.png';
 import styles from './Navbar.module.css';
+import Link from 'next/link';
 
 const Navlogo = () => {
   return (
     <div className={styles.outer}>
       <div className={styles.left}>
-        <Image src={logo} alt="logo" layout="responsive" />
+        <Link href="/">
+          <Image src={logo} alt="logo" layout="responsive" />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navlogo
+export default Navlogo;
